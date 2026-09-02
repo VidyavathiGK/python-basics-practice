15d9ec082ff63ccc8868c7553075a114248bd05e
Vidyavathi GK <vidyavidyavathigk@gmail.com>
2026-09-02 23:13:32 +0530

    Implement matrix addition in matrix_addition.py

---
+++ matrix_addition.py
@@ -0,0 +1,8 @@
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+
+result = [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+print("Matrix Addition Result:")
+for row in result:
+    print(row)
